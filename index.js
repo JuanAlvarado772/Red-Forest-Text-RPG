55af2c8e4d9d9f72518a3c9efff97bc24919b700
Juan Alvarado <jca.alvaradojuan@gmail.com>
2024-02-22 00:31:37 -0500

    1

---
+++ index.js
@@ -0,0 +1,8 @@
+const newgame = document.getElementById('newgame');
+const startscreen = document.querySelector('.startscreen')
+const game = document.getElementById('game');
+
+newgame.addEventListener('click', function() {
+  startscreen.classList.toggle('hide')
+});
+
